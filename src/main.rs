fe4e4765fbec3945ce31291bfe7ef0d8133994b3
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-10-15 14:39:25 +0200

    feat(timewarp): added

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use clap::Parser;
+use std::fs::{DirEntry, File};
+use std::path::PathBuf;
+use std::time::Duration;
+use std::time::SystemTime;
+use std::{fs, io};
+use time::{Date, Month, OffsetDateTime, Time};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    /// Path to files
+    #[clap(short, long, value_name = "PATH_TO_FILE")]
+    path: String,
+
+    /// offset in seconds
+    #[arg(short = 'o', long, default_value_t = 1)]
+    offset: u64,
+
+    /// year to set
+    #[arg(short = 'y', long, default_value_t = 0)]
+    year: i32,
+
+    /// month to set
+    #[arg(short = 'm', long, default_value_t = 1)]
+    month: u8,
+
+    /// day to set
+    #[arg(short = 'd', long, default_value_t = 1)]
+    day: u8,
+
+    /// hour to set
+    #[arg(short = 'k', long, default_value_t = 0)]
+    hour: u8,
+
+    /// minute to set
+    #[arg(short = 'l', long, default_value_t = 0)]
+    minute: u8,
+
+    /// second to set
+    #[arg(short = 's', long, default_value_t = 0)]
+    second: u8,
+}
+
+fn main() -> io::Result<()> {
+    let cli = Cli::parse();
+    let mut entries = list_files(&cli.path)?;
+    entries.sort_by_key(|e| e.file_name());
+    let date = Date::from_calendar_date(
+        cli.year,
+        Month::try_from(cli.month).expect("Incorrect Month entered"),
+        cli.day,
+    )
+    .expect("Incorrect day entered");
+    let time = Time::from_hms(cli.hour, cli.minute, cli.second).expect("Incorrect time entered");
+    let current = OffsetDateTime::now_utc();
+    let datetime = OffsetDateTime::new_in_offset(date, time, current.offset());
+    let mut system_time = SystemTime::from(datetime);
+    let offset = Duration::from_secs(cli.offset);
+    for entry in entries {
+        let file = File::open(entry.path())?;
+        file.set_modified(system_time)?;
+        system_time = system_time.checked_add(offset).expect("Could not increase time");
+    }
+    println!("DONE");
+    Ok(())
+}
+
+pub fn list_files<T: Into<PathBuf>>(path: T) -> io::Result<Vec<DirEntry>> {
+    Ok(fs::read_dir(path.into())?.map_while(Result::ok).collect())
+}
